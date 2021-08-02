f9e30c1e5214d848e47a9102c49942921ec10750
Vanza Setia <venusbumi2@gmail.com>
2021-08-02 12:05:32 +0700

    Minified script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,1 @@
-const yearDOM = document.querySelector(".js-year")
-
-document.addEventListener("DOMContentLoaded", getCurrentYear)
-
-function getCurrentYear() {
-  const currentYear = new Date().getFullYear()
-  yearDOM.textContent = currentYear
-}
+const yearDOM=document.querySelector(".js-year");document.addEventListener("DOMContentLoaded",getCurrentYear);function getCurrentYear(){const currentYear=new Date().getFullYear();yearDOM.textContent=currentYear}
